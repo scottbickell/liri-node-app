614e0a9a801ce664a18c931e288393330bf4003e
Scott Bickell <bickellworks@gmail.com>
2019-02-20 19:11:52 -0700

    Initial commit in new repo for liribot

---
+++ liri.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+var fs = require("fs");
+var keys = require("./keys.js");
+var request = require('request');
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+
+var cmd = process.argv[2];
+var arg = process.argv[3];
+
